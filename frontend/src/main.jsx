4e7db9ff932b5ae0009fe4b20e22e3193407c034
vgnilrem <velasqmg@gmail.com>
2025-08-24 18:08:20 -0700

    Add Account page and update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,9 @@ import "./index.css";
 import Nav from "./components/Nav.jsx";
 import Home from "./pages/Home.jsx";
 // import Shows from "./pages/Shows.jsx";
-// import Playlists from "./pages/Playlists.jsx";
+// import Playlists from "./pages/Playlists.jsx"; 
+import Account from "./pages/account.jsx"; 
+//this is for now and will be connected to login page instead//
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -15,7 +17,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/shows" element={<Shows />} />
-        <Route path="/playlists" element={<Playlists />} /> */}
+        <Route path="/playlists" element={<Playlists />} /> */}3
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
